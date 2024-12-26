27e9e1ef8d605f59904e746f84a49e0bbcba902d
bondybondbond <bondybondbond@gmail.com>
2024-12-26 23:34:26 +0000

    fix: resolve hydration error by using dynamic import

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import MovieFeed from '../components/MovieFeed';
+import dynamic from 'next/dynamic'
+
+const MovieFeed = dynamic(() => import('../components/MovieFeed'), {
+  ssr: false
+})
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-lg mx-auto">
-        {/* @ts-expect-error Server Component */}
         <MovieFeed />
       </div>
     </div>
